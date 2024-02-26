4395395e1e41142baed6141563eeb08fedb285a8
Christian Rodriguez <rodchris3090452@gmail.com>
2024-02-26 14:12:39 -0500

    Enhanced navabr component

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -4,7 +4,7 @@ import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testi
 
 const App = () => (
   < div className='bg-primary w-full overflow-hidden' >
-    <div className={`${styles.paddingx} ${styles.flexCenter}`}>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
